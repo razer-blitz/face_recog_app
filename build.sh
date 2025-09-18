2a4fae37da85be4546ba09152055b07c8cf5a23b
razer-blitz <tomh10311@gmail.com>
2025-09-18 16:24:03 +0530

    Add build.sh for dlib-bin installation

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install dlib-bin==19.24.2
+pip install --no-deps git+https://github.com/razer-blitz/face_recognition.git face-recognition-models>=0.3.0
+pip install Flask==2.3.3 python-dotenv==1.0.1 Pillow==10.4.0 numpy==1.26.2 supabase==1.0.0 gunicorn==20.1.0
